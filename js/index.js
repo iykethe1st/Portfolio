7653f6a8a0e887aa9fdc1cb5852b6c8f8d60e96a
Ikenna Udemezue <iykethe1st@gmail.com>
2022-08-23 22:20:37 +0100

    Collapsibles component commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const collapseItems = document.querySelectorAll(".collapsible");
+
+
+collapseItems.forEach((item) => {
+    item.addEventListener('click', function(){
+        this.classList.toggle("collapsible--expanded")
+    });
+ });
+
