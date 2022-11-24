7f89eaa12a95344b03e4cf3f7e4e5f5ac4b2cfd8
Ikenna Udemezue <iykethe1st@gmail.com>
2022-11-24 14:20:58 +0100

    hot fix

--- js/mailSend.js
+++
@@ -1,8 +0,0 @@
-let submitButton = document.getElementById("submit_form");
-let form = document.getElementById("email_form");
-form.addEventListener("submit", function(e){
-    setTimeout(function(){
-        submitButton.value = "Sending...";
-        submitButton.disabled = true;
-    }, 1)
-});
--- js/progress.js
+++
@@ -1,9 +0,0 @@
-$(".progress-bar span").each(function () {
-    $(this).animate(
-      {
-        width: $(this).attr("data-progress") + "%"
-      },
-      1000
-    );
-  });
-  
